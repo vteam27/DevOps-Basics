28a574c76c16ac07ef2ba49bf66b663e3d226d1e
vteam27 <tiwarivaibhavin@gmail.com>
2025-01-03 23:52:56 +0530

    Add unit testing with JUnit

---
+++ AppTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+    @Test
+    public void testMain() {
+        assertEquals(1, 1, "Basic test to check CI setup");
+    }
+}
