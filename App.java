1e72ccec780bbcfaacd1ebc2dd5ef45436f8c301
vteam27 <tiwarivaibhavin@gmail.com>
2025-01-31 00:55:26 +0530

    added dev branch

--- App.java
+++ App.java
@@ -1,5 +1,8 @@
 public class App {
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println(
+            "Hello, World! This is a simple Java application. Let's add a frontend (separate)"
+        );
     }
 }
